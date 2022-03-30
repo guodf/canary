10af6ceff220cf06bd3297ea1acaa2d25bde0cf6
guodf <guodanfeng@antiy.cn>
2022-03-30 15:41:36 +0800

    update

--- mapper/README.md
+++ mapper/README.md
@@ -1,1 +1,10 @@
-# mapper
+# mapping
+
+用于A to B的转换
+
+## 静态映射 Source Code
+
+## 动态映射 reflect
+
+## 自定义标签
+
--- mapper/mapper_test.go
+++ mapper/mapper_test.go
@@ -7,16 +7,35 @@ import (
 	"e.coding.net/guodf/gopkg/goutil/mapper"
 )
 
+type SubDto struct {
+	Field1 string
+	Field2 int
+}
+
 // HelloDto dto
 type HelloDto struct {
 	Field1 string
-	field2 string
+	Field2 int
+	Field3 bool
+	Field4 byte
+	Field5 []byte
+	Field6 SubDto
+	Field7 *SubDto
+	Field8 []*SubDto
+	Field9 map[string]interface{}
 }
 
 // HelloModel model
 type HelloModel struct {
 	Field1 string
-	field2 string
+	Field2 int
+	Field3 bool
+	Field4 byte
+	Field5 []byte
+	Field6 SubDto
+	Field7 *SubDto
+	Field8 []*SubDto
+	Field9 map[string]interface{}
 }
 
 func TestDtoToModel(t *testing.T) {
