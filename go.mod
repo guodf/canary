47717e1465e4894422ad72ea663568321696ef36
guodf <guo_net_win@live.com>
2023-06-13 23:24:50 +0800

    添加websocket工具类

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/gorilla/websocket v1.5.0
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
@@ -16,7 +17,7 @@ require (
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
+	github.com/stretchr/testify v1.8.3 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
---
+++ net/wsocket.go
@@ -0,0 +1,115 @@
+package net
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	Closed = errors.New("WebSocket is closed")
+)
+
+type Event struct {
+	EventType string      `json:"eventType"`
+	Data      interface{} `json:"data"`
+}
+
+type wsocketClient struct {
+	Url     string
+	Header  *http.Header
+	handers map[string]EventHandlerFunc
+	conn    *websocket.Conn
+	isConn  bool
+}
+
+type wsocketServer struct {
+	w              http.ResponseWriter
+	r              *http.Request
+	conn           *websocket.Conn
+	responseHeader *http.Header
+	handers        map[string]EventHandlerFunc
+	isConn         bool
+	mutex          sync.Mutex
+}
+
+type EventHandlerFunc func(event *Event)
+
+type IWSocket interface {
+	Watcher(eventType string, handler EventHandlerFunc)
+	Run() error
+	Close()
+	Send(event Event) error
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func NewWSocketServer(w http.ResponseWriter, r *http.Request, responseHeader *http.Header) IWSocket {
+	return &wsocketServer{
+		conn:           nil,
+		w:              w,
+		r:              r,
+		responseHeader: responseHeader,
+		handers:        map[string]EventHandlerFunc{},
+		isConn:         false,
+	}
+}
+
+func (ws *wsocketServer) Watcher(eventType string, handler EventHandlerFunc) {
+	ws.handers[eventType] = handler
+}
+
+func (ws *wsocketServer) Run() error {
+	c, err := upgrader.Upgrade(ws.w, ws.r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return err
+	}
+	ws.conn = c
+	ws.isConn = true
+	ws.listen()
+	return nil
+}
+
+func (ws *wsocketServer) listen() {
+	defer func() {
+		log.Println("close conn")
+		if ws.conn != nil {
+			ws.conn.Close()
+		}
+		err := recover()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+	for {
+		var event Event
+		err := ws.conn.ReadJSON(&event)
+		if err != nil {
+			log.Println("read:", err)
+			return
+		}
+		go ws.handers[event.EventType](&event)
+	}
+}
+
+func (ws *wsocketServer) Close() {
+	ws.isConn = false
+	ws.conn.Close()
+}
+
+func (ws *wsocketServer) Send(event Event) error {
+	if ws.isConn {
+		ws.mutex.Lock()
+		defer ws.mutex.Unlock()
+		return ws.conn.WriteJSON(event)
+	}
+	return Closed
+}
