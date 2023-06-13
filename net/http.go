6193a62858dd260e57ca11374f4bd4c8f302ec1e
guodf <guo_net_win@live.com>
2023-06-13 23:24:35 +0800

    添加忽略tls函数

---
+++ net/http.go
@@ -0,0 +1,15 @@
+package net
+
+import (
+	"crypto/tls"
+	"net/http"
+)
+
+var tr = &http.Transport{
+	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+}
+
+func IgnoreTls(client *http.Client) *http.Client {
+	client.Transport = tr
+	return client
+}
